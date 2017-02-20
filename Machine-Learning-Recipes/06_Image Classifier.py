de20e5a0f7de015a08fc40c96805243fc307ed42
Benjamin Huang <benjaminhuanghuang@gmail.com>
2017-02-19 18:01:58 -0800

    update

--- Machine-Learning-Recipes/06_Image Classifier.py
+++ Machine-Learning-Recipes/06_Image Classifier.py
@@ -1,4 +1,21 @@
 '''
 TensorFlow for Poets
+TF Learn : High level ML library on top of TensorFlow
 
 '''
+
+from sklearn import metrics, cross_validation
+import tensorflow as tf
+from tensorflow.contrib import learn
+
+
+def main(unused_argv):
+    iris = learn.datasets.load_dataset('iris')
+    X_train, X_test, y_train, y_test = \
+        cross_validation.train_test_split(iris.data, iris.target, test_size=0.2, random_state=42)
+
+    classifier = learn.DNNClassifier(hidden_units=[10, 20, 10], n_classes=3)
+
+    classifier.fit(X_train, y_train, steps=200)
+    score = metrics.accuracy_score(y_test, classifier.predict(X_test))
+    print "Accuracy: {0:f}".format(score)
---
+++ Machine-Learning-Recipes/07_Classifying Handwritten Digits.py
@@ -0,0 +1,10 @@
+'''
+https://www.youtube.com/watch?v=Gj0iyo265bc
+1. Installation
+2. Download dataset
+3. Visualize images
+4. Train a classifier
+5. Evaluate
+6. Visualize weights
+
+'''
