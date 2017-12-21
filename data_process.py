56fc986b94ede65fd0f65842fd28fb4ec6049a4b
Benjamin Huang <bhuang@rms.com>
2017-12-20 18:02:59 -0800

    data process

---
+++ data_process.py
@@ -0,0 +1,36 @@
+import os
+import tarfile
+from six.moves import urllib
+import pandas as pd
+
+
+DOWNLOAD_ROOT = "https://raw.githubusercontent.com/ageron/handson-ml/master/"
+HOUSING_PATH = "datasets/housing"
+HOUSING_URL = DOWNLOAD_ROOT + HOUSING_PATH + "/housing.tgz"
+
+'''
+creates a datasets/housing directory in workspace, downloads the housing.tgz file, 
+and extracts the housing.csv from it.
+'''
+
+
+def fetch_housing_data(housing_url=HOUSING_URL, housing_path=HOUSING_PATH):
+    if not os.path.isdir(housing_path):
+        os.makedirs(housing_path)
+    tgz_path = os.path.join(housing_path, "housing.tgz")
+    urllib.request.urlretrieve(housing_url, tgz_path)
+    housing_tgz = tarfile.open(tgz_path)
+    housing_tgz.extractall(path=housing_path)
+    housing_tgz.close()
+
+
+def load_housing_data(housing_path=HOUSING_PATH):
+ csv_path = os.path.join(housing_path, "housing.csv")
+ return pd.read_csv(csv_path)
+
+
+
+if __name__ == '__main__':
+    fetch_housing_data()
+    housing = load_housing_data()
+    print(housing.head())   # return first 5 rows
