c32844fde1aaf7963a2df70d94f17659ecfc3a00
Benjamin Huang <bhuang@rms.com>
2018-03-26 14:41:47 -0700

    plot house

--- housing.py
+++ housing.py
@@ -12,7 +12,7 @@ import tarfile
 from six.moves import urllib
 import pandas as pd
 import numpy as np
-from sklearn.model_selection import train_test_split
+from sklearn.model_selection import StratifiedShuffleSplit
 
 import matplotlib
 matplotlib.use('TkAgg')
@@ -50,7 +50,20 @@ if __name__ == '__main__':
     print('Value categories of ocean_proximity', housing['ocean_proximity'].value_counts())
     print('Data description', housing.describe())    # statistic information
     
-    housing.hist(bins=50, figsize=(20,15))
-    plt.show()
+    # housing.hist(bins=50, figsize=(20,15))
+    # plt.show()
 
-    train_set, test_set = train_test_split(housing, test_size=0.2, random_state=42)
+    housing["income_cat"] = np.ceil(housing["median_income"] / 1.5)
+    # merging all the categories greater than 5 into category 5
+    housing["income_cat"].where(housing["income_cat"] < 5, 5.0, inplace=True)
+    split = StratifiedShuffleSplit(n_splits=1, test_size=0.2, random_state=42)
+    for train_index, test_index in split.split(housing, housing["income_cat"]):
+        strat_train_set = housing.loc[train_index]
+        strat_test_set = housing.loc[test_index]
+
+    for set in (strat_train_set, strat_test_set):
+        set.drop(["income_cat"], axis=1, inplace=True)
+
+    housing = strat_train_set.copy()
+    housing.plot(kind="scatter", x="longitude", y="latitude")
+    plt.show()
