8ec6f3659f9112a7fdf75194ddc9c8459fc2e61e
Benjamin Huang <bhuang@rms.com>
2018-03-26 19:27:48 -0700

    update

--- KNN/knn_digits.py
+++ KNN/knn_digits.py
@@ -13,14 +13,27 @@ digits = datasets.load_digits()
 X = digits.data
 y = digits.target
 
+neighbors = np.arange(1, 9)
+train_accuracy = np.empty(len(neighbors))
+test_accuracy = np.empty(len(neighbors))
 
 X_train, X_test, y_train, y_test = train_test_split(
     X, y, test_size=0.3, random_state=21, stratify=y)
 
-knn = KNeighborsClassifier(n_neighbors=5)
-knn.fit(X_train, y_train)
+for i, k in enumerate(neighbors):
+    knn = KNeighborsClassifier(n_neighbors=k)
+    knn.fit(X_train, y_train)
 
-y_pred = knn.predict(X_test)
+    #Compute accuracy on the training set
+    train_accuracy[i] = knn.score(X_train, y_train)
+    #Compute accuracy on the testing set
+    test_accuracy[i] = knn.score(X_test, y_test)
 
-# The accuracy
-score = knn.score(X_test, y_test)
+# Generate plot
+plt.title('k-NN: Varying Number of Neighbors')
+plt.plot(neighbors, test_accuracy, label = 'Testing Accuracy')
+plt.plot(neighbors, train_accuracy, label = 'Training Accuracy')
+plt.legend()
+plt.xlabel('Number of Neighbors')
+plt.ylabel('Accuracy')
+plt.show()
